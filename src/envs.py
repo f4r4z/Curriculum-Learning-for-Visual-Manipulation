10385aeb3297f0c3656eb85cac1b1b6cd315c456
f4r4z <farazmh1999@gmail.com>
2024-04-01 15:44:49 -0400

    adding HER training script and goal environment for HER

--- src/envs.py
+++ src/envs.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Union
+from collections import OrderedDict
 import gymnasium as gym
 import numpy as np
 from gymnasium.spaces import Box
@@ -19,6 +20,7 @@ class AgentViewEnv(OffScreenRenderEnv):
 
     def step(self, action):
         obs, reward, done, info = super().step(action)
+        print(obs)
         success = self.check_success()
         reward = 10.0 * success
         self.step_count += 1
@@ -30,6 +32,81 @@ class AgentViewEnv(OffScreenRenderEnv):
         obs = super().reset()
         self.step_count = 0
         return obs["agentview_image"]
+
+
+class AgentViewGoalEnv(OffScreenRenderEnv):
+    """ Sparse reward environment with only the agent view as observation for HER.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # desired goal to close microwave
+        close_threshold = 0.0 # taken from articulated_objects.py Microwave object default close ranges
+        self.desired_goal = np.array([close_threshold])
+
+        agent_view_shape = self.env._get_observations()["agentview_image"].shape
+        self.observation_space = self._make_observation_space(agent_view_shape, self.desired_goal.shape)
+        print(self.desired_goal.shape, self.observation_space)
+
+        self.action_space = Box(low=-1, high=1, shape=(7,), dtype="float32")
+        self.step_count = 0
+
+    def _get_obs(self):
+        """
+        Helper to create the observation
+        """
+        # Since object is microwave, we will only have one joint
+        for joint in self.env.get_object(self.obj_of_interest).joints:
+            qpos_addr = self.env.sim.model.get_joint_qpos_addr(joint)
+            qpos = self.env.sim.data.qpos[qpos_addr]
+
+        return OrderedDict(
+            [
+                ("observation", self.agentview),
+                ("achieved_goal", np.array([qpos])), # TODO: numpy or 1 value is fine?
+                ("desired_goal", self.desired_goal) # TODO: do we sample desired goal here?
+            ]
+        )
+    
+    def _make_observation_space(self, agent_view_shape, goal_shape):
+        return gym.spaces.Dict(
+            {
+                "observation": Box(low=0, high=255, shape=agent_view_shape, dtype="uint8"),
+                "achieved_goal": Box(low=-np.inf, high=np.inf, shape=goal_shape, dtype="uint8"),
+                "desired_goal": Box(low=-np.inf, high=np.inf, shape=goal_shape, dtype="uint8"),
+            }
+        )
+
+    def step(self, action):
+        obs, reward, done, info = super().step(action)
+        
+        self.agentview = obs["agentview_image"]
+        observation = self._get_obs()
+        success = self.check_success()
+        reward = self.compute_reward(obs['achieved_goal']. obs['desired_goal'], _info = None)
+        self.step_count += 1
+        truncated = self.step_count > 250
+        done = success or truncated
+        # TODO: return agentview or observation? if observation, then _get_obs need to return agentview_image as "observation"
+        info = {"is_success": done, "agentview_image": self.agentview}
+        return observation, reward, done, truncated, info
+
+    def compute_reward(
+            self, achieved_goal, desired_goal, _info = None
+    ) -> np.float32:
+        close_ranges = [-0.005, 0.0]
+        close_range = np.array([abs(close_ranges[1] - close_ranges[0])])
+
+        if desired_goal - close_range < achieved_goal < desired_goal + close_range:
+            return 10.0
+        else:
+            return 0.0
+
+    def reset(self):
+        obs = super().reset()
+        self.agentview = obs["agentview_image"]
+        self.step_count = 0
+        return self._get_obs()
 
 
 class LowDimensionalObsEnv(OffScreenRenderEnv):
