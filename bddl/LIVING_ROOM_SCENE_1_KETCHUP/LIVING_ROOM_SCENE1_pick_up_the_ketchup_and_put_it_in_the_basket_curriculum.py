Fix bounding box size calculation and make some start tasks for ketchup curriculum easier

bddl/LIVING_ROOM_SCENE_1_KETCHUP/LIVING_ROOM_SCENE1_pick_up_the_ketchup_and_put_it_in_the_basket_curriculum.py
@@ -111,11 +111,11 @@ def lift_the_ketchup():
 
 def align_the_ketchup():
     bddl = base_bddl.format("(And (Align ketchup_1 basket_1 {}) (Lift ketchup_1 basket_1 0.02))")
-    return [bddl.format(align_distance) for align_distance in np.arange(0.4, -0.0001, -0.05)]
+    return [bddl.format(align_distance) for align_distance in np.arange(0.6, -0.0001, -0.05)]
 
 def lower_the_ketchup_to_the_basket():
     bddl = base_bddl.format("(And (Proximity ketchup_1 basket_1 {}) (Align ketchup_1 basket_1))")
-    return [bddl.format(align_distance) for align_distance in np.arange(0.2, -0.00001, -0.01)]
+    return [bddl.format(align_distance) for align_distance in np.arange(0.4, -0.00001, -0.01)]
 
 def put_the_ketchup_in_the_basket():
     return base_bddl.format("(And (In ketchup_1 basket_1_contain_region) (Not (Grasp ketchup_1)))")

src/libero_utils.py
@@ -86,7 +86,7 @@ def box_bounds(size: np.ndarray, position: np.ndarray, rotation: np.ndarray):
         [-1, -1, 1],
         [-1, -1, -1],
     ])
-    points = convert_coords(size)/2 * unit_cube
+    points = convert_coords(size) * unit_cube
     points = points @ rotation + position
     # points = (size/2 * unit_cube) @ rotation + position
     return points.min(axis=0), points.max(axis=0)